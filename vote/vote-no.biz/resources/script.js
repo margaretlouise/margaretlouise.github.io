d07ceea7ce110ed6cd2145887303176e0b94eadd
Margaret Adams <mla@lclark.edu>
2016-03-31 00:56:52 -0700

    what

---
+++ vote/vote-no.biz/resources/script.js
@@ -0,0 +1,4 @@
+document.getElementById("box").onclick = function() {
+  document.getElementById("check").style.display = "inline-block";
+  document.getElementById("paypal").style.display = "block"
+};
